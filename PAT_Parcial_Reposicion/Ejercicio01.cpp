509fbec24af04c25fab41af0f4759b464c482e2d
dummy2004 <p.klee.4bdb@gmail.com>
2023-11-11 22:46:03 -0600

    no sirve el codigo solo es una base para guiarme

--- PAT_Parcial_Reposicion/Ejercicio01.cpp
+++ PAT_Parcial_Reposicion/Ejercicio01.cpp
@@ -2,6 +2,32 @@
 
 Node<int>* Ejercicio01::addTwoNumbers(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2)
 {
-    
+    int carry = 0;
+    Node<int>* dummy = new Node<int>(0);
+    Node<int>* curr = dummy;
+
+    while (sizeL1 > 0 || sizeL2 > 0) {
+        int x = (sizeL1 > 0) ? l1->data : 0;
+        int y = (sizeL2 > 0) ? l2->data : 0;
+        int sum = carry + x + y;
+        carry = sum / 10;
+
+        curr->next = new Node<int>(sum % 10);
+        curr = curr->next;
+
+        if (sizeL1 > 0) {
+            l1 = l1->next;
+            sizeL1--;
+        }
+        if (sizeL2 > 0) {
+            l2 = l2->next;
+            sizeL2--;
+        }
+    }
+    if (carry > 0) {
+        curr->next = new Node<int>(carry);
+    }
+
+    return dummy->next;
     return nullptr;
 }
